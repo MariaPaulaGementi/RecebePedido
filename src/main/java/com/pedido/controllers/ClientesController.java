d6cc444af3a18e580cc668190348da26f901fe37
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2023-11-21 16:44:53 -0300

    Criando os endPoints de buscar pelo id e inserir para os clientes

--- src/main/java/com/pedido/controllers/ClientesController.java
+++ src/main/java/com/pedido/controllers/ClientesController.java
@@ -1,11 +1,10 @@
 package com.pedido.controllers;
 
 import com.pedido.entities.Cliente;
+import com.pedido.entities.Pedido;
 import com.pedido.repositories.ClientesRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,17 @@ public class ClientesController {
     public List<Cliente> findAll(){
         List<Cliente> listClientes =  repository.findAll();
         return listClientes;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Cliente findById(@PathVariable Long id){
+        Cliente clienteId =  repository.findById(id).get();
+        return clienteId;
+    }
+
+    @PostMapping
+    public Cliente insert(@RequestBody Cliente clientes){
+        Cliente saveCliente =  repository.save(clientes);
+        return saveCliente;
     }
 }
