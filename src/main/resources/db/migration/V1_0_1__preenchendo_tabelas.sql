4e45ba28c749b98ee6dda35d99c8aae21cfdc67f
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2023-11-21 18:53:02 -0300

    Criando scripts de insert no banco.

---
+++ src/main/resources/db/migration/V1_0_1__preenchendo_tabelas.sql
@@ -0,0 +1,37 @@
+
+insert INTO tb_cliente values ( 1, '0001');
+insert INTO tb_cliente values ( 2, '0002');
+insert INTO tb_cliente values ( 3, '0003');
+
+insert Into tb_pedido values (
+    1,
+    25984,
+    '2023-02-01',
+    'produto 1',
+    2,
+    '0001'
+)
+insert Into tb_pedido values (
+    2,
+    24569,
+    '2023-11-01',
+    'produto 2',
+    5,
+    '0002'
+)
+insert Into tb_pedido values (
+    3,
+    244569,
+    '2023-08-01',
+    'produto 3',
+    9,
+    '0002'
+)
+insert Into tb_pedido values (
+    4,
+    14454,
+    '2023-12-01',
+    'produto 4',
+    3,
+    '0003'
+)
